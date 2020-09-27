41008fef9c715d7a9694bd470416dbfa37be7b3b
Gavin Alexander <info@ignytsolutions.com>
2020-09-27 10:40:32 +0200

    Restructured Utilities

--- MyLayercake.Utilities/Extensions/IEnumerableExtensions.cs
+++ MyLayercake.Utilities/Extensions/IEnumerableExtensions.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace MyLayercake.Utilities {
+namespace MyLayercake.Utilities.Extensions {
     public static class IEnumerableExtensions {
         public static IEnumerable<T> ForEach<T>(this IEnumerable<T> coll, Action<T> func) {
             foreach (var v in coll) func?.Invoke(v);
--- MyLayercake.Utilities/Helpers/DirectoryHelper.cs
+++ MyLayercake.Utilities/Helpers/DirectoryHelper.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.IO;
 
-namespace MyLayercake.Utilities {
+namespace MyLayercake.Utilities.Helpers {
     public static class DirectoryHelper {
         public static string GetDataDirectory() {
             return Path.Combine(Directory.GetParent(AppDomain.CurrentDomain.BaseDirectory).Parent.Parent.FullName, "Data");
